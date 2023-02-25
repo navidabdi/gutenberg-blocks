d28960419349c08b0f31d0e69e61b8674b73a52b
Nabi <artsanaat@gmail.com>
2023-02-25 21:05:45 +0300

    Completed the boilerplate.

---
+++ inc/Plugin.php
@@ -0,0 +1,96 @@
+<?php
+
+namespace Naviddev\GutenbergBlocks;
+
+class Plugin {
+	/**
+	 * Prefix for naming.
+	 *
+	 * @var string
+	 */
+	const PREFIX = 'naviddev-gutenberg-blocks';
+
+	/**
+	 * Gettext localization domain.
+	 *
+	 * @var string
+	 */
+	const L10N = self::PREFIX;
+
+	/**
+	 * @var string
+	 */
+	private static string $baseUrl;
+
+	public static function perInit(): void {
+		// block initialization
+		add_action('init',  __CLASS__ . '::gutenbergBlocksInit');
+	}
+
+	public static function init(): void {
+		// blocks category
+		if (version_compare($GLOBALS['wp_version'], '5.7', '<')) {
+			add_filter('block_categories', __CLASS__ . '::gutenbergBlocksRegisterCategory', 10, 2);
+		}
+		else {
+			add_filter('block_categories_all', __CLASS__ . '::gutenbergBlocksRegisterCategory', 10, 2);
+		}
+	}
+
+	public static function gutenbergBlocksRegister($name, $options = []): void {
+		register_block_type(self::getBasePath() . '/build/blocks/' . $name, $options);
+	}
+
+	public static function gutenbergBlocksInit(): void {
+		self::gutenbergBlocksRegister('bootstrap');
+		self::gutenbergBlocksRegister('block-test');
+	}
+
+	public static function gutenbergBlocksRegisterCategory($categories, $post): array {
+		return [
+			[
+				'slug'  => 'naviddev-gutenberg-blocks',
+				'title' => __('NavidDev Gutenberg Blocks', Plugin::L10N),
+			],
+			...$categories,
+		];
+	}
+
+	public static function gutenbergBlocksExternalLibraries() {
+		wp_enqueue_script(
+			'gutenberg-blocks-lib',
+			GUTENBERG_BLOCKS_INC_URL . 'js/plugin.js',
+			[],
+			GUTENBERG_BLOCKS_VERSION,
+			TRUE
+		);
+	}
+
+	/**
+	 * Loads the plugin text domain.
+	 */
+	public static function loadTextDomain(): void {
+		load_plugin_textdomain(static::L10N, FALSE, static::L10N . '/languages/');
+	}
+
+	/**
+	 * The base URL path to this plugin's folder.
+	 *
+	 * Uses plugins_url() instead of plugin_dir_url() to avoid a trailing slash.
+	 */
+	public static function getBaseUrl(): string {
+		if (!isset(static::$baseUrl)) {
+			static::$baseUrl = plugins_url('', static::getBasePath() . '/plugin.php');
+		}
+		return static::$baseUrl;
+	}
+
+	/**
+	 * The absolute filesystem base path of this plugin.
+	 *
+	 * @return string
+	 */
+	public static function getBasePath(): string {
+		return dirname(__DIR__);
+	}
+}
--- plugin.php
+++ plugin.php
@@ -3,7 +3,7 @@
  * Plugin Name:       Gutenberg Blocks
  * Description:       An addon for Gutenberg plugin.
  * Requires at least: 6.1
- * Requires PHP:      7.0
+ * Requires PHP:      7.4
  * Version:           0.1.0
  * Author:            Nabi Abdi
  * Author URI:        https://naviddev.com
@@ -12,82 +12,32 @@
  * Text Domain:       naviddev-gutenberg-blocks
  */
 
-// Stop Direct Access 
-if (!defined('ABSPATH')) {
-	exit;
-}
+ namespace Naviddev\GutenbergBlocks;
 
-/**
- * Blocks Final Class
- */
+ if (!defined('ABSPATH')) {
+   header($_SERVER['SERVER_PROTOCOL'] . ' 404 Not Found');
+   exit;
+ }
 
-final class GutenbergBlocks {
+ define('GUTENBERG_BLOCKS_VERSION', '0.1.0');
+ define('GUTENBERG_BLOCKS_URL', plugin_dir_url( __FILE__ ));
+ define('GUTENBERG_BLOCKS_INC_URL', GUTENBERG_BLOCKS_URL . 'includes/');
 
-	public function __construct() {
-
-		// define constants
-		$this->gutenbergBlocksConstants();
-
-		// block initialization
-		add_action('init', [$this, 'gutenbergBlocksInit']);
-
-		// blocks category
-		if (version_compare($GLOBALS['wp_version'], '5.7', '<')) {
-			add_filter('block_categories', [$this, 'gutenbergBlocksRegisterCategory'], 10, 2);
-		} 
-		else {
-			add_filter('block_categories_all', [$this, 'gutenbergBlocksRegisterCategory'], 10, 2);
-		}
-
-		// enqueue block assets
-		// add_action('enqueue_block_assets', [$this, 'gutenbergBlocksExternalLibraries']);
-	}
-
-	public static function init() {
-		static $instance = false; 
-		if(!$instance) {
-			$instance = new self();
-		}
-		return $instance;
-	}
-
-	private function gutenbergBlocksConstants() {
-		define('GUTENBERG_BLOCKS_VERSION', '0.1.0');
-		define('GUTENBERG_BLOCKS_URL', plugin_dir_url( __FILE__ ));
-		define('GUTENBERG_BLOCKS_INC_URL', GUTENBERG_BLOCKS_URL . 'includes/');		
-	}
-
-	public function gutenbergBlocksRegister($name, $options = []) {
-		register_block_type(__DIR__ . '/build/blocks/' . $name, $options);
+ /**
+	* Loads PSR-4-style plugin classes.
+	*/
+ function classloader($class) {
+	 static $ns_offset;
+	 if (strpos($class, __NAMESPACE__ . '\\') === 0) {
+		 if ($ns_offset === NULL) {
+			 $ns_offset = strlen(__NAMESPACE__) + 1;
+		 }
+		 include __DIR__ . '/inc/' . strtr(substr($class, $ns_offset), '\\', '/') . '.php';
 	 }
+ }
+ spl_autoload_register(__NAMESPACE__ . '\classloader');
 
-	public function gutenbergBlocksInit() {
-		$this->gutenbergBlocksRegister('bootstrap');
-		$this->gutenbergBlocksRegister('block-test');
-	}
-
-	public function gutenbergBlocksRegisterCategory($categories, $post) {
-		return array_merge(
-			[
-				[
-					'slug'  => 'naviddev-gutenberg-blocks',
-					'title' => __('NavidDev Gutenberg Blocks', 'naviddev-gutenberg-blocks'),
-				],
-			],
-			$categories,
-		);
-	}
-
-	public function gutenbergBlocksExternalLibraries() {
-		wp_enqueue_script(
-			'gutenberg-blocks-lib',
-			 GUTENBERG_BLOCKS_INC_URL . 'js/plugin.js',
-			 [],
-			 GUTENBERG_BLOCKS_VERSION,
-			 TRUE
-			);
-	}
-
-}
-
-GutenbergBlocks::init();
+ add_action('plugins_loaded', __NAMESPACE__ . '\Plugin::loadTextDomain');
+ add_action('init', __NAMESPACE__ . '\Plugin::perInit', 0);
+ add_action('init', __NAMESPACE__ . '\Plugin::init', 20);
+//add_action('admin_init', __NAMESPACE__ . '\Admin::init');
